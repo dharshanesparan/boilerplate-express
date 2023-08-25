fda2de13a2a4193ee0019fe7dacc0f784ac44b78
dharshanesparan <dharshanpa.esparan644@gmail.com>
2023-08-26 00:25:34 +0530

    update

--- myApp.js
+++ myApp.js
@@ -1,10 +1,13 @@
 let express = require('express');
+const path = require('path');
 let app = express();
 console.log("Hello World");
 
+const publicFolderPath = path.join(__dirname, 'public');
+app.use('/public', express.static(publicFolderPath));
 
 app.get('/', (req, res) => {
-    absolutePath = __dirname + '/views/index.html'
+    absolutePath = __dirname + '/public/style.css'
     res.sendFile(absolutePath);
   });
 
