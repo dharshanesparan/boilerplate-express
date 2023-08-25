550ab1a15a6be3401982a86a5d88b7fce1122ded
dharshanesparan <dharshanpa.esparan644@gmail.com>
2023-08-25 23:59:25 +0530

    update

--- myApp.js
+++ myApp.js
@@ -2,8 +2,11 @@ let express = require('express');
 let app = express();
 console.log("Hello World");
 
-module.exports = app;
 
+
+app.get('/', (req, res) => {
+    res.send('Hello Express');
+  });
 
 
 
